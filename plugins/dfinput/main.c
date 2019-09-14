6fc3b3b9a4a456126262f359bafaed3257422778
SindenLightgun <47393540+SindenLightgun@users.noreply.github.com>
2019-09-14 13:05:34 +0100

    new Guncon functionality

--- plugins/dfinput/main.c
+++ plugins/dfinput/main.c
@@ -40,10 +40,11 @@ static int old_controller_type1 = -1, old_controller_type2 = -1;
 			pad_init(); \
 			break; \
 		case PSE_PAD_TYPE_GUNCON: \
-			PAD##n##_startPoll = PADstartPoll_guncon; \
-			PAD##n##_poll = PADpoll_guncon; \
-			guncon_init(); \
-			break; \
+			//Removed for new Guncon functionality, may have been required for very old touchscreen support
+			//PAD##n##_startPoll = PADstartPoll_guncon; \
+			//PAD##n##_poll = PADpoll_guncon; \
+			//guncon_init(); \
+			//break; \
                 case PSE_PAD_TYPE_NEGCON: \
 		case PSE_PAD_TYPE_GUN: \
 		default: \
